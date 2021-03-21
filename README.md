a9a368e44259e10437b3e95b08d9e153f0235ae8
ulka008 <ulka008@gmail.com>
2021-03-21 01:05:59 -0500

    implemented flex-menu, bootstrap, modal picture box

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is my garden project, which includes some information on the herb garden we started with my children during a lockdown.
-It includes 10 common herbs, their pictures and germination days.
+It includes 10 common herbs, their pictures, latin names and germination days.
 
 
 
-I also created a sticky footer. I used 'calc' to get rid of the scroll bar 
+I also created a sticky footer. 
--- js/directory.js
+++ js/directory.js
@@ -1,13 +1,117 @@
-let html = '';
+var plants = [
+    {
+        name: 'Basil',
+        latin: 'Ocimum basilicum',
+        germination: '7-10 days to sprout',
+        photo: 'img/basil.jpg'
+    },
+    {
+        name: 'Chives',
+        latin: 'Allium schoenoprasum',
+        germination: '14-21 days to sprout',
+        photo: 'img/chives.jpg'
+    },
+    {
+        name: 'Cilantro',
+        latin: 'Coriandrum sativum',
+        germination: '7-10 days to sprout',
+        photo: 'img/cilantro.jpg'
+    },
+    {
+        name: 'Dill',
+        latin: 'Anethum graveolens',
+        germination: '10-14 days to sprout',
+        photo: 'img/dill.jpg'
+    },
+    {
+        name: 'Mint',
+        latin: 'Mentha',
+        germination: '10-16 days to sprout',
+        photo: 'img/mint.jpg'
+    },
+    {
+        name: 'Oregano',
+        latin: 'Origanum vulgare',
+        germination: '10-15 days to sprout',
+        photo: 'img/oregano.jpg'
+    },
+    {
+        name: 'Parsley',
+        latin: 'Petroselinum crispum',
+        germination: '14-30 days to sprout',
+        photo: 'img/parsley.jpg'
+    },
+    {
+        name: 'Rosemary',
+        latin: 'Salvia rosmarinus',
+        germination: '14-30 days to sprout',
+        photo: 'img/rosemary.jpg'
+    },
+    {
+        name: 'Sage',
+        latin: 'Salvia officinalis',
+        germination: '7-21 days to sprout',
+        photo: 'img/sage.jpg'
+    },
+    {
+        name: 'Thyme',
+        latin: 'Thymus vulgaris',
+        germination: '10-14 days to sprout',
+        photo: 'img/thyme.jpg'
+    }
+];
 
-for (let i = 0; i < plants.length; i ++) {
-    let plants = plants[i];
-    html += `
-        <h2>${plants.name}</h2>
-        <h3>${plants.type}</h3>
-        <p>${plants.germination}</p>
-        <img src="$plants.photo}"> 
-        `;
-}
+// var zoomInPlant = function(plantName) {
+//     let plant = plants.find(p => p.name === plantName );
+//     console.log(plant);
+//     let zoomedImg = $("#zoomInImage");    
+//     zoomedImg.html(`
+//         <img src="${plant.photo}"> 
+//     `);
 
-document.querySelector('main').insertAdjacentHTML('beforeend', html);
+//     zoomedImg[0].classList.add('show-float-center');
+//     zoomedImg[0].classList.remove('hide');
+
+
+
+//     let plantEl = $(`#${plantName}`)[0]
+//     plantEl.classList.toggle('zoom-in');
+
+// }
+
+
+
+  
+
+
+
+$(document).ready(function() {
+    $('#modalImageBox').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var plantName = button.data('plant-name') // Extract info from data-* attributes   
+        let plant = plants.find(p => p.name === plantName ); 
+        var modal = $(this);
+        modal.find('.modal-title').text(plantName)
+        modal.find('#zoomInImage').html(`
+            <img src="${plant.photo}"> 
+        `);    
+      });
+    
+    let html = '';
+
+    for (let i = 0; i < plants.length; i ++) {
+        let plant = plants[i];
+        /* <div id='${plant.name}' class='clearfix plant-canvas' onclick="zoomInPlant('${plant.name}')">       */
+        html += `
+            <div id='${plant.name}' class='clearfix plant-canvas' ">                              
+                <div class='plant-name'>${plant.name}</div>
+                <div class='plant-type'>${plant.latin}</div>
+                <div class='plant-germination'>${plant.germination}</div>                  
+                <img src="${plant.photo}" data-toggle='modal' data-target='#modalImageBox' data-plant-name='${plant.name}' "> 
+            </div>
+            `;
+    }
+      
+    $('#herbsList').html(html);
+});
+

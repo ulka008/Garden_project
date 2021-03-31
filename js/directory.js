474e281b41c4d938e8ad0f2be7abc8cb82015918
ulka008 <ulka008@gmail.com>
2021-03-31 12:25:11 -0500

    cleaned the file

--- js/directory.js
+++ js/directory.js
@@ -47,48 +47,24 @@ var plants = [
         germination: '14-30 days to sprout',
         photo: 'img/rosemary.jpg'
     },
-    {
-        name: 'Sage',
-        latin: 'Salvia officinalis',
-        germination: '7-21 days to sprout',
-        photo: 'img/sage.jpg'
-    },
-    {
-        name: 'Thyme',
-        latin: 'Thymus vulgaris',
-        germination: '10-14 days to sprout',
-        photo: 'img/thyme.jpg'
-    }
+    // {
+    //     name: 'Sage',
+    //     latin: 'Salvia officinalis',
+    //     germination: '7-21 days to sprout',
+    //     photo: 'img/sage.jpg'
+    // },
+    // {
+    //     name: 'Thyme',
+    //     latin: 'Thymus vulgaris',
+    //     germination: '10-14 days to sprout',
+    //     photo: 'img/thyme.jpg'
+    // }
 ];
-
-// var zoomInPlant = function(plantName) {
-//     let plant = plants.find(p => p.name === plantName );
-//     console.log(plant);
-//     let zoomedImg = $("#zoomInImage");    
-//     zoomedImg.html(`
-//         <img src="${plant.photo}"> 
-//     `);
-
-//     zoomedImg[0].classList.add('show-float-center');
-//     zoomedImg[0].classList.remove('hide');
-
-
-
-//     let plantEl = $(`#${plantName}`)[0]
-//     plantEl.classList.toggle('zoom-in');
-
-// }
-
-
-
-  
-
-
 
 $(document).ready(function() {
     $('#modalImageBox').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var plantName = button.data('plant-name') // Extract info from data-* attributes   
+        var button = $(event.relatedTarget) // Button that triggers the modal
+        var plantName = button.data('plant-name') // Extracts info from data-* attributes   
         let plant = plants.find(p => p.name === plantName ); 
         var modal = $(this);
         modal.find('.modal-title').text(plantName)
@@ -101,21 +77,22 @@ $(document).ready(function() {
 
     for (let i = 0; i < plants.length; i ++) {
         let plant = plants[i];
-        /* <div id='${plant.name}' class='clearfix plant-canvas' onclick="zoomInPlant('${plant.name}')">       */
+        
         html += `
         <table>
             <thead>
                 <tr id='name'>
+                <th scope="row">Name:</th>
                 <th scope="col">${plant.name}</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">Latin Name: </th>
+                    <th scope="row">Latin Name:</th>
                     <td>${plant.latin}</td>
                 </tr>
                 <tr>
-                    <th scope="row">Germination</th>
+                    <th scope="row">Germination:</th>
                     <td>${plant.germination}</td>
                 </tr>
                 <tr>

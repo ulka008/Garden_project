77ac05c36e558bda1a29c93d484b28c53c00d67e
ulka008 <ulka008@gmail.com>
2021-07-19 18:24:23 -0500

    validation correction

--- js/form.js
+++ js/form.js
@@ -1,69 +1,69 @@
 function validateName() {
 
-    var name = document.getElementById('contact-name').value("input");
+    var name = document.getElementById('contact_name').value;
 
     if(name.length == 0) {
 
-      producePrompt('Name is required', 'name-error' , 'red')
+      producePrompt('Name is required', 'name_error' , 'red')
       return false;
 
   }
 
   if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
 
-      producePrompt('First name, please.','name-error', 'red');
+      producePrompt('First name, please.','name_error', 'red');
       return false;
 
   }
 
-  producePrompt('Valid', 'name-error', 'green');
+  producePrompt('Valid', 'name_error', 'green');
   return true;
 
 }
 
 function validateEmail () {
 
-var email = document.getElementById('contact-email').value("input");
+var email = document.getElementById('contact_email').value;
 
 if(email.length == 0) {
 
-  producePrompt('Email Invalid','email-error', 'red');
+  producePrompt('Email Invalid','email_error', 'red');
   return false;
 
 }
 
 if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
 
-  producePrompt('Email Invalid', 'email-error', 'red');
+  producePrompt('Email Invalid', 'email_error', 'red');
   return false;
 
 }
 
-producePrompt('Valid', 'email-error', 'green');
+producePrompt('Valid', 'email_error', 'green');
 return true;
 
 }
 
 function validateMessage() {
-var message = document.getElementById('contact-message').value("input");
+var message = document.getElementById('contact_message').value;
 var required = 30;
 var left = required - message.length;
 
 if (left > 0) {
-  producePrompt(left + ' more characters required','message-error','red');
+  producePrompt(left + ' more characters required','message_error','red');
   return false;
 }
 
-producePrompt('Valid', 'message-error', 'green');
+producePrompt('Valid', 'message_error', 'green');
 return true;
 
 }
 
 function validateForm() {
 if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
-  jsShow('submit-error');
-  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
-  setTimeout(function(){jsHide('submit-error');}, 2000);
+  jsShow('submit_error');
+  producePrompt('Please fix errors to submit.', 'submit_error', 'red');
+  setTimeout(function(){jsHide('submit_error');}, 2000);
   return false;
 }
 else {

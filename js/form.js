a547d237d6ddc3fc66da889cee4fb062a4a8e484
ulka008 <ulka008@gmail.com>
2021-07-21 20:15:15 -0500

    added regex to email

--- js/form.js
+++ js/form.js
@@ -6,19 +6,21 @@ function validateName() {
 
       producePrompt('Name is required', 'name_error' , 'red')
       return false;
-
   }
 
   if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
 
       producePrompt('First name, please.','name_error', 'red');
       return false;
-
   }
 
   producePrompt('Valid', 'name_error', 'green');
   return true;
+}
 
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
 }
 
 function validateEmail () {
@@ -29,19 +31,16 @@ if(email.length == 0) {
 
   producePrompt('Email Invalid','email_error', 'red');
   return false;
-
 }
 
-if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+if(!isEmail(email)) {
 
   producePrompt('Email Invalid', 'email_error', 'red');
   return false;
-
 }
 
 producePrompt('Valid', 'email_error', 'green');
 return true;
-
 }
 
 function validateMessage() {
@@ -56,18 +55,16 @@ if (left > 0) {
 
 producePrompt('Valid', 'message_error', 'green');
 return true;
-
 }
 
 function validateForm() {
-if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+if (!validateName() || !validateEmail() || !validateMessage()) {
   jsShow('submit_error');
   producePrompt('Please fix errors to submit.', 'submit_error', 'red');
   setTimeout(function(){jsHide('submit_error');}, 2000);
   return false;
 }
 else {
-
 }
 }
 
@@ -84,5 +81,4 @@ function producePrompt(message, promptLocation, color) {
 
 document.getElementById(promptLocation).innerHTML = message;
 document.getElementById(promptLocation).style.color = color;
-
 }
